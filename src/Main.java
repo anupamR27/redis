ead27655ab4450165a01ef2e1616082fbcc955af
anupamR27 <172539307+anupamR27@users.noreply.github.com>
2026-09-01 13:39:24 +0530

    Accept client connection

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(6379);
 
         System.out.println("Mini Redis server started on port 6379");
+        System.out.println("Waiting for a client");
+
+        Socket clientSocket = serverSocket.accept();
+
+        System.out.println("Client connected");
+
+        clientSocket.close();
+        serverSocket.close();
+
 
         while (true) {
             // Keep the server running for now
