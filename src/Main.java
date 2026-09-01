fa0994873aabb30b84aaeb8406687e261feb786e
anupamR27 <172539307+anupamR27@users.noreply.github.com>
2026-09-01 13:57:44 +0530

    Handle plain text PING command

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -7,18 +10,29 @@ public class Main {
         ServerSocket serverSocket = new ServerSocket(6379);
 
         System.out.println("Mini Redis server started on port 6379");
-        System.out.println("Waiting for a client");
+        System.out.println("Waiting for a client...");
 
         Socket clientSocket = serverSocket.accept();
 
         System.out.println("Client connected");
 
+        BufferedReader input = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream())
+        );
+        
+        // This lets the server read text from the client.
+        PrintWriter output = new PrintWriter(clientSocket.getOutputStream(), true);
+        // This lets the server send text to the client.
+
+        String command = input.readLine();
+
+        if ("PING".equalsIgnoreCase(command)) {
+            output.println("PONG");
+        } else {
+            output.println("Unknown command");
+        }
+
         clientSocket.close();
         serverSocket.close();
-
-
-        while (true) {
-            // Keep the server running for now
-        }
     }
 }
